07f840f625a4d98857172a9e4c97c49b977991b7
StuartCharters <charters.stuart@gmail.com>
2018-05-29 16:37:59 +0100

    setup river.spec

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,6 +1,17 @@
 require("minitest/autorun")
 require_relative("../river.rb")
+require_relative("../fish.rb")
 
 class RiverTest < MiniTest::Test
+
+  def setup
+    @fish1 = Fish.new("Joe")
+    @fish2 = Fish.new("Sarah")
+    @river1 = River.new("Amazon")
+  end
+
+  def test_river_name
+    assert_equal("Amazon",@river1.name())
+  end
 
 end
