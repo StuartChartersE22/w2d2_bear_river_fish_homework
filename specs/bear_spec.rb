dd904d91496a838a7f6f276d32ccb235b41e7378
StuartCharters <charters.stuart@gmail.com>
2018-05-29 16:52:58 +0100

    setup bear_spec

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -3,4 +3,12 @@ require_relative("../bear.rb")
 
 class BearTest < MiniTest::Test
 
+  def setup
+    @bear1 = Bear.new("Yogi")
+  end
+
+  def test_bear_name
+    assert_equal("Yogi", @bear1.name())
+  end
+
 end
